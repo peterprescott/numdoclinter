a4238261a1efa40196ccef28df95fb0129337108
Peter Prescott <prescott.peter@gmail.com>
2022-03-29 00:02:04 +0100

    black

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -17,7 +17,7 @@ def func1(
     c: Union[float, int],
     d: Dict[str, List[int]],
     e: Optional[pd.DataFrame],
-) -> Optional[Union[dict,List[Union[float, int]]]]:
+) -> Optional[Union[dict, List[Union[float, int]]]]:
     """
     This is a Numpy style docstring.
 
@@ -42,6 +42,7 @@ def func1(
     """
     return None
 
+
 def get_func_ast(func):
     [func_ast] = ast.parse(getsource(func)).body
     return func_ast
@@ -58,46 +59,47 @@ def test_AnnotationInfo():
 
     func_ast = get_func_ast(func1)
     parsed_annotations = [
-            parse.AnnotationInfo(arg.annotation).txt
-            for arg in func_ast.args.args]
-    assert parsed_annotations[0] == 'int'
-    assert parsed_annotations[1] == 'str'
-    assert parsed_annotations[2] == 'Union[float, int]'
-    assert parsed_annotations[3] == 'Dict[str, List[int]]'
-    assert parsed_annotations[4] == 'Optional[pd.DataFrame]'
+        parse.AnnotationInfo(arg.annotation).txt
+        for arg in func_ast.args.args
+    ]
+    assert parsed_annotations[0] == "int"
+    assert parsed_annotations[1] == "str"
+    assert parsed_annotations[2] == "Union[float, int]"
+    assert parsed_annotations[3] == "Dict[str, List[int]]"
+    assert parsed_annotations[4] == "Optional[pd.DataFrame]"
 
 
 def test_DocstringInfo_description():
     docstring_info = get_DocstringInfo(func1)
-    assert docstring_info.description == 'This is a Numpy style docstring.'
+    assert (
+        docstring_info.description == "This is a Numpy style docstring."
+    )
 
 
 def test_DocstringInfo_param_descriptions():
     docstring_info = get_DocstringInfo(func1)
-    assert docstring_info.params[0]['desc'] == 'An integer.'
+    assert docstring_info.params[0]["desc"] == "An integer."
 
 
 def test_DocstringInfo_type_hinting():
     docstring_info = get_DocstringInfo(func1)
-    hints = {d['name']:d['hint'] for d in docstring_info.params}
-    assert hints['a'] == 'int'
-    assert hints['b'] == 'str'
-    assert hints['c'] == 'Union[float, int]'
-    assert hints['d'] == 'Dict[str, List[int]]'
-    assert hints['e'] == 'Optional[pd.DataFrame]'
+    hints = {d["name"]: d["hint"] for d in docstring_info.params}
+    assert hints["a"] == "int"
+    assert hints["b"] == "str"
+    assert hints["c"] == "Union[float, int]"
+    assert hints["d"] == "Dict[str, List[int]]"
+    assert hints["e"] == "Optional[pd.DataFrame]"
 
 
 def test_DocstringInfo_returns():
     docstring_info = get_DocstringInfo(func1)
     r = docstring_info.returns
-    assert r['hint'] == 'Optional[Union[dict, List[Union[float, int]]]'
-    assert r['desc'] == 'A weirdly complicated return type.'
+    assert r["hint"] == "Optional[Union[dict, List[Union[float, int]]]"
+    assert r["desc"] == "A weirdly complicated return type."
 
 
 def test_FunctionInfo():
     func_ast = get_func_ast(func1)
     fi = parse.FunctionInfo(func_ast, None, None)
-    assert fi.name == 'func1'
-    assert fi.args == ['a','b','c','d','e']
-
-
+    assert fi.name == "func1"
+    assert fi.args == ["a", "b", "c", "d", "e"]
