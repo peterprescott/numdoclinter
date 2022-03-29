a231d90feb99e581b02c567592ba190c999ace02
Peter Prescott <prescott.peter@gmail.com>
2022-03-29 22:53:30 +0100

    Use Fire to run numdoclinter from CLI

--- src/numdoclinter/actions.py
+++ src/numdoclinter/actions.py
@@ -3,6 +3,7 @@ import os
 from typing import List, Optional
 
 import pandas as pd
+import fire
 
 from numdoclinter.linter import Linter
 from numdoclinter.parse import FunctionInfo
@@ -83,3 +84,67 @@ def get_docstring_problems(folder, start=os.getcwd(), ignore_init=True):
     )
 
     return df
+
+
+def run_from_cli(
+    folder: str,
+    save_as_csv: bool = False,
+    csv_name: str = "DOCSTRING_PROBLEMS.csv",
+    echo_problems: bool = True,
+):
+    """
+    Run numdoclinter from CLI.
+
+    Parameters
+    ----------
+    folder: str
+        The folder that you want to search
+        (recursively) for modules (and nested subfolders containing
+        modules) containing functions, to check if their signatures and
+        docstrings conform to Numpy style.
+    save_as_csv: bool
+        Whether or not to save the list of problems as a CSV.
+    csv_name: str
+        If you have chosen to `save_as_csv`, the name of the CSV (which
+        will be saved in your current working directory).
+    echo_problems: bool
+        Whether to print the list of problems to your CLI.
+    """
+
+    location = os.getcwd()
+
+    funcs = list_funcs_recursively(location, folder, [])
+
+    funcs = [f for f in funcs if f.ast.name != "__init__"]
+    print(f"Found {len(funcs)} functions...")
+
+    full_list = [
+        (p, f, f.module, f.context.replace(location, ""))
+        for f in funcs
+        for p in Linter(f).problems
+    ]
+    print(f"... with {len(full_list)} docstring problems.")
+
+    if list_problems:
+        [
+            print(f"{os.path.join(x[3],x[2])}\n{x[1]}()\n{x[0]}\n")
+            for x in full_list
+        ]
+
+    print(
+        f"Summary: {len(funcs)} functions, {len(full_list)} problems."
+    )
+
+    if save_as_csv:
+        df = pd.DataFrame(
+            full_list,
+            columns=["problem", "function", "module", "context"],
+        )
+
+        df.to_csv(csv_name, index=False)
+        print(f"List saved as {csv_name}.")
+
+
+if __name__ == "__main__":
+
+    fire.Fire(run_from_cli)
