7c0d89f9a5017e87d59f21ab95db8fdbf4daac84
Peter Prescott <prescott.peter@gmail.com>
2022-03-29 23:09:10 +0100

    Delete WIP notebook

--- notebooks/hack.py
+++
@@ -1,9 +0,0 @@
-from sphinx.ext.napoleon import NumpyDocstring
-from numdoclinter import parse, actions, linter
-
-start = '/home/peterprescott/90poe/data-science-common'
-folder = 'data_science_common'
-
-funcs = actions.list_files_recursively(start, folder, [])
-
-len([f for f in funcs if linter.Linter(f).problems])
