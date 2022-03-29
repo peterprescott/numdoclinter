e8cc4086be1918df15bdbd3df2ee549170953403
Peter Prescott <prescott.peter@gmail.com>
2022-03-29 17:47:57 +0100

    Test no_description_in_docstring

--- tests/test_linter.py
+++ tests/test_linter.py
@@ -7,6 +7,16 @@ from numdoclinter import linter, parse, actions
 def with_no_docstring():
     pass
 
+def with_no_description(x:int):
+    """
+    Parameters
+    ----------
+    x: int
+        An integer.
+    """
+    pass
+
+
 
 def get_FunctionInfo_object(func):
     [func_ast] = ast.parse(getsource(func)).body
@@ -17,3 +27,9 @@ def get_FunctionInfo_object(func):
 def test_no_docstring():
     func_with_no_docstring = get_FunctionInfo_object(with_no_docstring)
     assert linter.no_docstring(func_with_no_docstring)
+    func_with_no_description = get_FunctionInfo_object(with_no_description)
+    assert not linter.no_docstring(func_with_no_description)
+
+def test_no_description_in_docstring():
+    func_with_no_description = get_FunctionInfo_object(with_no_description)
+    assert linter.no_description_in_docstring(func_with_no_description)
